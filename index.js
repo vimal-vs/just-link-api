4419969871ff6b8009c4ce266a244a683564a2b0
Vimal Vs <vimalchan24@gmail.com>
2023-06-21 20:58:37 +0530

    api/index removed

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "../routes/auth.js";
-import userRoutes from "../routes/users.js";
-import postRoutes from "../routes/posts.js";
-import { register } from "../controllers/auth.js";
-import { createPost } from "../controllers/posts.js";
-import { verifyToken } from "../middleware/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
